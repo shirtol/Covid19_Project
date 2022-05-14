755f86f45bc0532d62475e85f152dc345a5b94b0
shirtol <shirzahavi7@gmail.com>
2022-05-14 23:28:01 +0300

    Added selector base class

--- script/ContinentSelector.js
+++ script/ContinentSelector.js
@@ -1,29 +1,14 @@
-import { Observable } from "./Observable.js";
+import { Selector } from "./Selector.js";
 
-/**
- * @class
- */
-export class ContinentSelector {
+export class ContinentSelector extends Selector {
     constructor() {
-        this.allContinentsBtn = document.querySelectorAll("[data-continent]");
-        this.dropdownBtn = document.querySelector(".drop-btn");
-        this.selectedContinent = new Observable("Africa");
-        this.addEventToAllContinents();
+        super("continent", "Africa", "Choose Country");
+        this.selectedContinent = this.selectedValue;
         this.changeSelectedContinentElement();
     }
 
-    addEventToAllContinents = () => {
-        this.allContinentsBtn.forEach((continentBtn) => {
-            continentBtn.addEventListener("click", (e) => {
-                this.selectedContinent.value =
-                    continentBtn.getAttribute("data-continent");
-                this.dropdownBtn.firstChild.textContent = "Choose Country";
-            });
-        });
-    };
-
     changeSelectedContinentElement = () => {
-        this.selectedContinent.addChangeListener((newSelectedContinent) => {
+        this.selectedValue.addChangeListener((newSelectedContinent) => {
             document.querySelector(".selected").classList.remove("selected");
             document
                 .querySelector(`[data-continent=${newSelectedContinent}]`)
--- script/CountrySelector.js
+++ script/CountrySelector.js
@@ -1,21 +1,8 @@
-import { Observable } from "./Observable.js";
+import { Selector } from "./Selector.js";
 
-export class CountrySelector {
+export class CountrySelector extends Selector {
     constructor() {
-        this.allCountriesBtn = document.querySelectorAll("[data-country]");
-        this.dropdownBtnTxt = document.querySelector(".drop-btn");
-        this.selectedCountry = new Observable("");
-        this.addEventToAllCountries();
+        super("country", "");
+        this.selectedCountry = this.selectedValue;
     }
-
-    addEventToAllCountries = () => {
-        this.allCountriesBtn.forEach((countryBtn) => {
-            countryBtn.addEventListener("click", (e) => {
-                this.selectedCountry.value =
-                    e.target.getAttribute("data-country");
-                this.dropdownBtnTxt.firstChild.textContent =
-                    this.selectedCountry.value;
-            });
-        });
-    };
 }
