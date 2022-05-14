56186ce7034a72d696f53592bf5fda5c59051c51
shirtol <shirzahavi7@gmail.com>
2022-05-15 00:34:42 +0300

    Removed console.log

--- script/index.js
+++ script/index.js
@@ -6,8 +6,4 @@ document.querySelector(".fa-sync").addEventListener("click", () => {
 
 const countryController = await CountryController.build();
 
-console.log(countryController);
-
 countryController.initializeView();
-
-console.log(countryController.continents);
